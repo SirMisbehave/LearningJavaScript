decea2479e36e096738f2c8df3f0ab6d0623000e
A2Misbehave <hello@a2misbehave.com>
2025-03-27 01:49:37 +1300

    JS Statements

---
+++ 01-W3-JS-Statements.js
@@ -0,0 +1,52 @@
+/*
+
+    Author: 	A2Misbehave
+    Resource: 	W3 Schools - JavaScript
+    URL:		https://www.w3schools.com/js/js_statements.asp
+
+*/
+
+// Statements
+
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+
+console.log(x, ",", y, "The sum of x and y:", z);
+
+// Semicolons
+
+let a, b, c;    // Declare 3 Variables
+a = 5;          // Assign the value 5 to a.
+b = 6;          // Assign the value 6 to b.
+c = a + b;      // Assign the sum of a and b to c.
+
+console.log(a, ",", b, "The sum of a and b:", c);
+
+// Semicolons - One Line
+
+a = 5; b = 6; c = a + b;
+
+// JavaScript White Space
+
+let person1 = "Hege";
+let person2="Hege";
+
+// Good Practices - Spaces Around Operators
+
+let x2 = y + z;
+
+/*
+
+    var         Declares a variable.
+    let         Declares a block variable.
+    const       Declares a block constant.
+    if          Marks a block of statements to be executed on a condition.
+    switch      Marks a block of statements to be executed in different cases.
+    for         Marks a block of statements to be executed in a loop.
+    function    Declares a function.
+    return      Exits a function.
+    try         Implements error handling to a block of statements.
+
+*/
