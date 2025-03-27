802961c4bb08ae0e36420fbce0be348144e73e12
A2Misbehave <hello@a2misbehave.com>
2025-03-28 03:15:09 +1300

    JS Operators

---
+++ 06-W3-JS-Operators.js
@@ -0,0 +1,63 @@
+/*
+
+    Author: 	SirMisbehave
+    Resource: 	W3 Schools - JavaScript
+    URL:		https://www.w3schools.com/js/js_operators.asp
+
+*/
+
+/*
+
+    Assignment Operator     - = -   Assigns Values
+    Addition Operator       - + -   Adds Values
+    Multiplication Operator - * -   Multiplies Values
+    Comparison Operator     - > -   Compares Values
+
+*/
+
+// JavaScript Assignment
+
+let x = 5
+let y = 2
+let z = x + y
+
+console.log("Assignment:", z)
+
+// JavaScript Addition
+
+let x2 = 5
+let y2 = 2
+let z2 = x2 + y2
+
+console.log("Addition:", z2)
+
+// JavaScript Multiplication
+
+let x3 = 5
+let y3 = 2
+let z3 = x3 * y3
+
+console.log("Multiplication:", z3)
+
+// JavaScript Arithmetic Operators
+
+let a = 3
+let x4 = (100 + 50) * a
+
+console.log("Arithmetic Operators:", x4)
+
+// JavaScript String Comparison
+
+let text1 = "A"
+let text2 = "B"
+let result = text1 < text2
+
+console.log("String Comparison:", result)
+
+// JavaScript String Addition
+
+let text1again = "John"
+let text2again = "Doe"
+let text3again = text1again + " " + text2again
+
+console.log("String Addition:", text3again)
