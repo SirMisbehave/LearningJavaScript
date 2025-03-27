37004cdc15299766ea9973997985db026bcd4d85
A2Misbehave <hello@a2misbehave.com>
2025-03-28 03:35:41 +1300

    JS Assignment

---
+++ 07-W3-JS-Arithmetic.js
@@ -0,0 +1,90 @@
+/*
+
+    Author: 	SirMisbehave
+    Resource: 	W3 Schools - JavaScript
+    URL:		https://www.w3schools.com/js/js_arithmetic.asp
+*/
+
+/*
+
+    Arithmetic Operators
+
+    +   Addition
+    -   Subtraction
+    *   Multiplication
+    **  Exponentiation (ES2016)
+    /   Division
+    %   Modulus (Remainder)
+    ++  Increment
+    --  Decrement
+
+*/
+
+// Arithmetic Operations
+
+let x = 100 + 50
+
+console.log("Arithmetic Operations:", x)
+
+// Adding
+
+let x1 = 5
+let y1 = 2
+let z1 = x1 + y1
+
+console.log("Adding:", z1)
+
+// Subtracting
+
+let x2 = 5
+let y2 = 2
+let z2 = x2 - y2
+
+console.log("Subtracting:", z2)
+
+// Multiplying
+
+let x3 = 5
+let y3 = 2
+let z3 = x3 * y3
+
+console.log("Multiplying:", z3)
+
+// Dividing
+
+let x4 = 5
+let y4 = 2
+let z4 = x4 / y4
+
+console.log("Dividing:", z4)
+
+// Remainder
+
+let x5 = 5
+let y5 = 2
+let z5 = x5 % y5
+
+console.log("Remainder:", z5)
+
+// Incrementing
+
+let x6 = 5
+x6++
+let z6 = x6
+
+console.log("Incrementing:", z6)
+
+// Decrementing
+
+let x7 = 5
+x7--
+let z7 = x7
+
+console.log("Decrementing:", z7)
+
+// Exponentiation
+
+let x8 = 5
+let z8 = x8 ** 2
+
+console.log("Exponentiation:", z8)
