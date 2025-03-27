039bde1c5f3855ac060eab27108cfb52b71571c9
A2Misbehave <hello@a2misbehave.com>
2025-03-27 23:44:27 +1300

    JS Variables Completed

--- 03-W3-JS-Variables.js
+++ 03-W3-JS-Variables.js
@@ -16,6 +16,7 @@
     y = 6;
     z = x + y;
 
+    Now according to W3 this is supposed to work automatically with the JavaScript detecting what it should be based on value? But I'm using Bun runtime, and it isn't allowing it. Potentially as a result of default TypeScript support, some rules are being applied even though I haven't configured it for TypeScript development.
 */
 
 // Declaration Using Var
@@ -57,7 +58,45 @@ let person = "John Doe", carName = "Volvo", price = 200;
 
 console.log("Person, Car Name & Price:", person, ",", carName, ",", price);
 
-// JavaScript Arithmetic
+// JavaScript Arithmetic - Algebra
 
 let x = 5 + 2 + 3;
 console.log("Arithmetic:", x);
+
+// JavaScript Arithmetic - Strings
+
+let x5 = "John" + " " + "Doe";
+console.log("Arithmetic Strings:", x5);
+
+// JavaScript Arithmetic - Mixed
+
+// Also testing removing semi-colons as I think Bun runtime is allowing it very much like 
+// TypeScript does, however this means I can't declare multiple variables on a single line. Which I 
+// prefer to keep things nice, clean and readable.
+
+let x6 = "5" + 2 + 3
+console.log("Arithmetic Mixed:", x6) 
+
+// The above is going to output '523' since due to one of the values being a string type, it treats 
+// them all as the same data type.
+
+// The following will add the first two as a number data type, then take that answer and 
+// concantenate to the last string type value.
+let x7 = 2 + 3 + "5"
+console.log("Arithmetic Mixed Alternative:", x7)
+
+// JavaScript Dollar Sign - An alias for main function in JavaScript library.
+
+let $ = "Hello World"
+let $$$ = 2
+let $myMoney = 5
+
+console.log($, $$$, $myMoney)
+
+// JavaScript Underscore - An alias for hidden variables.
+
+let _lastName = "Johnson"
+let _x = 2
+let _100 = 5
+
+console.log(_lastName, _x, _100)
