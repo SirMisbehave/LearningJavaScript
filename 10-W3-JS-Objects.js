e764790633c3c432d6c6dab5d958c4423feaf8fc
A2Misbehave <hello@a2misbehave.com>
2025-03-30 03:04:44 +1300

    JS Objects

---
+++ 10-W3-JS-Objects.js
@@ -0,0 +1,52 @@
+/*
+
+    Author: 	SirMisbehave
+    Resource: 	W3 Schools - JavaScript
+    URL:		https://www.w3schools.com/js/js_objects.asp
+
+*/
+
+// Creating a JavaScript Object
+
+const person = { 
+    firstName: "John", 
+    lastName: "Doe", 
+    age: 50, 
+    eyeColor: "blue" 
+}
+
+console.log("Person", person)
+
+const person1 = {}
+
+person1.firstName = "John"
+person1.lastName = "Doe"
+person1.age = 50
+person1.eyeColor = "blue"
+
+console.log("Person 1:", person1)
+
+// Using the New Keyword
+
+const person2 = new Object()
+
+person2.firstName = "John"
+person2.lastName = "Doe"
+person2.age = 50
+person2.eyeColor = "blue"
+
+console.log("Person 2:", person2)
+
+// Objects Methods
+
+const person3 = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function() {
+        return this.firstName + " " + this.lastName
+    }
+}
+
+console.log(person3)
+
