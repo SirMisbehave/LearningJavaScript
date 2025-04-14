8ecdee2448568b6664654929526097bd11e77468
A2Misbehave <hello@a2misbehave.com>
2025-04-15 03:07:30 +1200

    JS Save Progress

---
+++ 16-W3-JS-String-Methods.js
@@ -0,0 +1,33 @@
+/*
+
+    Author: 	SirMisbehave
+    Resource: 	W3 Schools - JavaScript
+    URL:		https://www.w3schools.com/js/js_string_methods.asp
+
+*/
+
+// JavaScript String Methods
+
+// JavaScript String Length
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let length = text.length
+
+/*  
+    Extract String Characters
+
+    4 Methods for Extracting String Characters:
+    - 'at(position)' Method
+    - 'charAt(position) Method
+    - 'charCodeAt(position) Method
+    - Using property access [] like in arrays.
+
+*/
+
+// JavaScript String charAt()
+
+let other_text = "HELLOzzzzzzzzz WORLD"
+let char = text.charAt(0)
+
+// JavaScript String charCodeAt()
+
+let char_2 = text.charg
